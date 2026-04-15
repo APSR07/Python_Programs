dab36a0e111c756542fd54081eee3830a1f329de
APSR07 <apsr725@gmail.com>
2026-04-15 18:44:39 +0530

    Largest number program upload

---
+++ 16_FindThreeLargest.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number = "))
+num2 = int(input("Enter second number = "))
+num3 = int(input("Enter third number = "))
+
+if(num1 > num2 and num1 > num3):
+    print("First number is greater")
+
+elif(num2 > num3):
+    print("Second number is greater ")
+
+else:
+    print("third number is greater ")
+
