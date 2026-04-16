8de706f1ebbdbf9f4aabd8c0030c9e20e8a8fb51
APSR07 <apsr725@gmail.com>
2026-04-16 05:47:48 +0530

    flag variable removed then update into new branch

--- 18_CheckPrime.py
+++ 18_CheckPrime.py
@@ -1,12 +1,14 @@
 num = int(input("enter a number for checking prime or not = "))
-flag = 0
 
-for i in range(2, num+1):
-    if(num % i == 0):
-        flag = flag + 1
-    
-if(flag == 1):
-    print("the number is prime")
+if num<2:
+    print("not prime")
 
 else:
-    print("not prime")
+        
+    for i in range(2, num):
+        if(num % i == 0):
+            print("not prime")
+            break
+
+    else:
+        print("prime")
